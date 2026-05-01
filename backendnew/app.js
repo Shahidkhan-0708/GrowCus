7ae8757b90b19faaf86d4226a3ecc40dc185264e
Shahidkhan-0708 <Shahidkhan07191@gmail.com>
2026-05-01 23:38:19 +0530

    url update

--- backendnew/app.js
+++ backendnew/app.js
@@ -18,7 +18,7 @@ const PORT=process.env.PORT
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-      origin:"http://127.0.0.1:4040",
+      origin:"https://growcus-new-17.onrender.com",
       credentials: true,
 }))
 app.use(cookieparser())
