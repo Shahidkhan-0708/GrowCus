9679df52ff2377edbd1b13c08c9e7c0fa05489f9
Shahidkhan-0708 <Shahidkhan07191@gmail.com>
2026-05-01 18:51:23 +0530

    cors problem

--- backendnew/app.js
+++ backendnew/app.js
@@ -18,7 +18,7 @@ const PORT=process.env.PORT
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-      origin: "http://localhost:3000",
+      origin: "https://growcus-new-9.onrender.com",
       credentials: true,
 }))
 app.use(cookieparser())
