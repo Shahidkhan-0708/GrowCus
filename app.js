a65f0ed02a44356e2f69ab6c1db42eda7ce78ba0
Shahidkhan-0708 <Shahidkhan07191@gmail.com>
2026-05-03 17:31:09 +0530

    fix :port

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ const limit=rateLimit({
 const cookieparser=require("cookie-parser");
 const {verifyToken}=require("./middlewares/auth")
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT|| 5000
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-      origin:"http://localhost:3000",
+      origin:"*",
       credentials: true,
 }))
 app.use(cookieparser())
@@ -51,16 +51,15 @@ app.use("/api/analytics",anaRoute);
 const server=async () => {
   try {
     await db()
-  app.listen(PORT,() => {
-  console.log("ur listening to port: ",PORT)
-}
-)
+  console.log("db connected")
+
   } catch (error) {
-    console.error("server not started because database connection failed")
+    console.error("DB failed, but starting server anyway")
     process.exit(1)
   }
+   app.listen(PORT, () => {
+    console.log("Server running on port", PORT)
+  })
 }
-server()
 
-
-
+server();
