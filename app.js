849611d2c41f6db35f994fad872725140cb1b4cf
Shahidkhan-0708 <Shahidkhan07191@gmail.com>
2026-05-24 01:29:54 +0530

    fix: set CORS origin to true for bulletproof cross-origin routing

--- app.js
+++ app.js
@@ -25,11 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(logger)
 app.use(cors({
-      origin: [
-        "http://localhost:3000", 
-        "http://localhost:3001",
-        process.env.CLIENT_URL
-      ].filter(Boolean),
+      origin: true,
       credentials: true,
 }))
 app.use(cookieparser())
